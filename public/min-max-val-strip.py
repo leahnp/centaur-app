d81555f42cc6d7cdfe517eca34255e9d0ee969e3
Leah <leahnpetersen@gmail.com>
2016-08-11 14:10:01 -0700

    data files

--- public/min-max-val-strip.py
+++ public/min-max-val-strip.py
@@ -6,7 +6,7 @@ local_max = [0.0, 0.0]
 
 for line in sys.stdin:
   split = line.split(' ')
-  secs = float(split[0]) * 10.0
+  secs = float(split[0]) * 1000.0
   accel = float(split[1]) # / -16384.0) + 1.0
  
   if not first and (last_accel >= 0.0) != (accel >= 0.0):
