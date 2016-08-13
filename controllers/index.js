07e10792fbc88b6cdbf2c4b0813dc7b5d0234dac
Leah <leahnpetersen@gmail.com>
2016-08-13 12:22:22 -0700

    filenames now save as type-time

--- controllers/index.js
+++ controllers/index.js
@@ -2,20 +2,26 @@ var fs = require("fs");
 
 var IndexController = {
   uploadData: function(req, res) {
-    // path is relative to cwd
-    // console.log(process.cwd());
-    var filename = "data/test.dat";
-    var data = req.body;
+    var data = req.body.data;
 
+    console.log(data)
     // convert javascript array to space delimeted
     // list of x, y, z accel values
     var output = "";
     for (var i = 0; i < data.length; i++) {
       var row = data[i];
-      output += row[0] + " " + row[1] + " " + row[2] + "\n";
+      output += row[0] + " " + row[1] + " " + row[2] + " " + row[3] +"\n";
     }
 
-    fs.writeFile(filename, output, function(error) {
+    // generate unique file name
+    var splitOutput = output.split(' ')
+    var filename = req.body.type + "-" + parseInt(splitOutput[0] + ".dat") 
+    // path is relative to cwd
+    // console.log(process.cwd());
+    var filepath = "data/" + filename;
+
+
+    fs.writeFile(filepath, output, function(error) {
        if (error) {  
          console.error("write error:  " + error.message);
          return;
@@ -24,26 +30,6 @@ var IndexController = {
        res.sendStatus(200);
     });
   }
-
-  // startCapture: function(req, res) {
-  //     // window.ondevicemotion = function(event) {  
-  //     //   var accelerationX = event.accelerationIncludingGravity.x;  
-  //     //   var accelerationY = event.accelerationIncludingGravity.y;  
-  //     //   var accelerationZ = event.accelerationIncludingGravity.z;  
-  //     // }  
-
-  //  media.remove(req.params.id, function(error) {
-  //    if(error=="Could not add to db") {
-  //      res.status(404).send(error)
-  //    } else if (error) {
-  //      // var err = "Please try again"
-  //      res.status(500).send(error)
-  //    } else {
-  //      console.log("final else")
-  //      res.redirect('/' + req.body.media)
-  //    }
-  //  })
-  // }
 }
 
 
