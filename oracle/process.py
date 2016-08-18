355a65f282563faded49a10003893a14849beb4f
Leah <leahnpetersen@gmail.com>
2016-08-18 13:46:04 -0700

    refactored

--- oracle/process.py
+++ oracle/process.py
@@ -4,10 +4,15 @@ import sys
 def run_process(data):
 	first_line = True
 	first_ms = 0
-	
+
 	for line in data:
 		split = line.rstrip().split(' ')
 		ms = float(split[0])
+
+		if sys.argv[1]:
+			label = sys.argv[1]
+		else:
+			label = 0
 
 		if first_line == True:
 		    first_ms = ms
@@ -16,13 +21,10 @@ def run_process(data):
 		split[0] = str((ms - first_ms) / 1000.0);
 
 		# new output to expand features
-		print('%s %s %s %s %s' % (split[0], split[1], split[2], split[3]))
+		print('%s %s %s %s %s' % (split[0], split[1], split[2], split[3], label))
 
 
 # for training data, runs if called from terminal
 if __name__ == '__main__':
-	label = sys.argv[1]
-	for line in sys.stdin:
-		split = line.rstrip().split(' ')
-	print('%s %s %s %s %s' % (split[0], split[1], split[2], split[3], label))
+	print(run_process(sys.stdin))
 
