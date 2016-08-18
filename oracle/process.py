7881705852806403a24276bc5aefe26f49353f7a
Leah <leahnpetersen@gmail.com>
2016-08-18 13:41:38 -0700

    altering to allow for slightly different functionality if called from terminal for training purposes or for user predict

--- oracle/process.py
+++ oracle/process.py
@@ -1,20 +1,28 @@
 import sys
 
-first_line = True
-first_ms = 0
-label = sys.argv[1]
+# for user predict data, runs when called from predict.py
+def run_process(data):
+	first_line = True
+	first_ms = 0
+	
+	for line in data:
+		split = line.rstrip().split(' ')
+		ms = float(split[0])
 
-for line in sys.stdin:
-	split = line.rstrip().split(' ')
+		if first_line == True:
+		    first_ms = ms
+		    first_line = False
 
-	ms = float(split[0])
+		split[0] = str((ms - first_ms) / 1000.0);
 
-	if first_line == True:
-	    first_ms = ms
-	    first_line = False
+		# new output to expand features
+		print('%s %s %s %s %s' % (split[0], split[1], split[2], split[3]))
 
-	split[0] = str((ms - first_ms) / 1000.0);
 
-	# new output to expand features
+# for training data, runs if called from terminal
+if __name__ == '__main__':
+	label = sys.argv[1]
+	for line in sys.stdin:
+		split = line.rstrip().split(' ')
 	print('%s %s %s %s %s' % (split[0], split[1], split[2], split[3], label))
 
