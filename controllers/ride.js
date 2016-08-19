fae8b79a97be8023b6132d73d712691b8376d8c0
Leah <leahnpetersen@gmail.com>
2016-08-19 07:35:35 -0700

    cleaned up code

--- controllers/ride.js
+++ controllers/ride.js
@@ -1,12 +1,33 @@
 var fs = require("fs");
-var rides = []
 
 var IndexController = {
-  saveData: function(req, res) {
+
+
+  index: function(req, res, next) {
+    fs.readdir('data', function(err, files) {
+      if (err) {
+        return next(err)
+      }
+      var rides = [];
+      for (var file of files) {
+        if (file.includes("user-")) {
+          rides.push(file)
+        }
+      }
+      res.render('ride', {rides: rides})
+    })
+  },
+
+  view: function(req, res) {
+    // var spawn = require('child_process').spawn,
+    // py    = spawn('python', ['../oracle/predict.py']),
+    // data = [1,2,3,4,5,6,7,8,9],
+    // dataString = '';
+
+  },
+
+  save: function(req, res, next) {
     var data = req.body.data;
-
-
-    // convert javascript array to space delimeted
     // list of x, y, z accel values
     var output = "";
     for (var i = 0; i < data.length; i++) {
@@ -14,39 +35,17 @@ var IndexController = {
       output += row[0] + " " + row[1] + " " + row[2] + " " + row[3] +"\n";
     }
 
-  
     // generate unique file name
-    var splitOutput = output.split(' ')
-    // var filename = req.body.type + "-" + parseInt(splitOutput[0] + ".dat") 
-    var filename = req.body.type + "-" + parseInt(splitOutput[0]) + ".dat"
-    // path is relative to cwd
-    // console.log(process.cwd());
-    var filepath = "oracle/predict_raww/" + filename;
+    var splitOutput = output.split(' ') 
+    var filename = "user-" + parseInt(splitOutput[0]) + ".dat"
+    var filepath = "data/" + filename;
 
-    rides.push(filename)
-
-    fs.writeFile(filepath, output, function(error) {
-       if (error) {  
-         console.error("write error:  " + error.message);
-         return;
-       } 
-      res.status(200).json({whatevs: 'it works!!!!'})
-        
-       // run python script, pass data?
+    fs.writeFile(filepath, output, function(err) {
+      if (err) {  
+        return next(err)
+      } 
+      res.status(200).json({});
     });
-  },
-
-  processData: function(req, res) {
-    var spawn = require('child_process').spawn,
-    py    = spawn('python', ['../oracle/predict.py']),
-    data = [1,2,3,4,5,6,7,8,9],
-    dataString = '';
-
-  },
-
-  showRidesData: function(req, res) {
-    res.render('rides', {rides: rides})
-
   }
 }
 
