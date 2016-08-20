bfd99db6c972511f6813c50a5bf66e65f2bb42a7
Leah <leahnpetersen@gmail.com>
2016-08-20 15:37:37 -0700

    cleaned up comments/old code

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,12 @@ var RideController = require('../controllers/ride')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.status(200).json({whatevs: 'it works!!!!'})
   res.render('index')
 });
 
 
 /* GET capture page. */
 router.get('/capture', function(req, res, next) {
-  // res.status(200).json({whatevs: 'it works!!!!'})
   res.render('capture')
 });
 
@@ -27,14 +25,5 @@ router.post('/ride/save', RideController.save)
 
 // take in data for training
 router.post('/upload_data', Controller.uploadData)
-
-// take in data from users for predicting
-// router.get('/rides', RideController.showRidesData)
-/* GET capture page. */
-// router.get('/rides', function(req, res, next) {
-//   // res.status(200).json({whatevs: 'it works!!!!'})
-//   res.render('rides')
-// });
-
 
 module.exports = router;
