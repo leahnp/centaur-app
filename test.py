ea7ee349259fd8c9dc99664550c4168d11f33b01
Leah <leahnpetersen@gmail.com>
2016-08-23 15:49:40 -0700

    test for deployment

---
+++ test.py
@@ -0,0 +1,10 @@
+import sys
+import numpy as np
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.externals import joblib 
+
+# for line in sys.stdin:
+# 	print line
+
+print "Hello World"
