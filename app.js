1f354c00e4bbf4376d2fb5b4cfb02b8534d7a9eb
Leah <leahnpetersen@gmail.com>
2016-08-23 15:47:21 -0700

    cleaned up for deployement

--- app.js
+++ app.js
@@ -1,54 +1,22 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var massive = require("massive");
 var app = module.exports = express();
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
-
-// var connectionString = "postgres://localhost/video_store"
-// var connectionString = "postgres://localhost/mediaranker" + app.get('env');
-var connectionString = "postgres://localhost/media_ranker_db";
-
-// connect to Massive and get the db instance. You can safely use the
-// convenience sync method here because its on app load
-// you can also use loadSync - it's an alias
-var massiveInstance = massive.connectSync({connectionString : connectionString})
-
-// Set a reference to the massive instance on Express' app:
-app.set('db', massiveInstance);
-
+var PORT = process.env.PORT || 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 // this will terminate the request with an error 400 if the POST body
 // doesn't contain valid json
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 var routes    = require('./routes/index');
-// var books = require('./routes/books');
-// var movies    = require('./routes/movies');
-// var albums   = require('./routes/albums');
-
-
 
 app.use('/', routes);
-// app.use('/books', books);
-// app.use('/movies', movies);
-// app.use('/albums', albums);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -80,6 +48,6 @@ app.use(function(req, res, next) {
 //     error: {}
 //   });
 // });
-
+app.listen(PORT);
 
 module.exports = app;
